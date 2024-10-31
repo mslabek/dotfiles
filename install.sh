13832db896a1ccc4ea7289c1a5a0bf1cf609a655
Mateusz Słabek <mslb@touk.pl>
2024-10-31 14:30:55 +0100

    wip

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 
 # TODO: ensure stow is installed
 
-stow --target "$HOME" bash
 stow --target "$HOME" --ignore='.config/tmux/plugins' tmux
 stow --target "$HOME" nvim
 stow --target "$HOME" alacritty
